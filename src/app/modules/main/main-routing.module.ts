2b44aed4a9115bf9e9f79c52478e9bfb6e4bbd9a
superpck@gmail.com <superpck@gmail.com>
2020-07-19 14:01:38 +0700

    main

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -6,24 +6,15 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
 const routes: Routes = [
   {
     path: 'main',
     component: LayoutComponent,
     children: [
-      {
-        path: '', component: MainPageComponent
-      },
-      {
-        path: 'main-page', component: MainPageComponent
-      },
-      {
-        path: 'about', component: AboutComponent
-      },
-      {
-        path: '**', component: PageNotFoundComponent
-      }
+      { path: '', component: MainPageComponent },
+      { path: 'main-page', component: MainPageComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '**', component: PageNotFoundComponent }
     ]
   }
 ];
