9232340e575c3efd04b27e32e7d22d56680f4bf7
superpck@gmail.com <superpck@gmail.com>
2020-07-21 23:09:02 +0700

    datagrid

--- src/app/modules/users/user-index/user-index.component.ts
+++ src/app/modules/users/user-index/user-index.component.ts
@@ -8,6 +8,7 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class UserIndexComponent implements OnInit {
   users: any = [];
+  loading = false;
 
   constructor(
     private userService: UsersService
@@ -18,11 +19,13 @@ export class UserIndexComponent implements OnInit {
   }
 
   async getUser() {
+    this.loading = true;
     const result: any = await this.userService.getUser();
     if (result['results']) {
       this.users = result['results'];
     } else {
       this.users = [];
     }
+    this.loading = false;
   }
 }
