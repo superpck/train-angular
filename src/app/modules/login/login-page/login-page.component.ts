f137d5a4331c362c55746e8d5c7220419b21e6bc
Pornchai Khamperngjai <pck@Pornchais-MacBook-Pro.local>
2020-07-31 21:54:07 +0700

    login page

--- src/app/modules/login/login-page/login-page.component.ts
+++ src/app/modules/login/login-page/login-page.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  username = '';
+  password = '';
 
   constructor() { }
 
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule } from '@angular/forms';
+import { ClarityModule } from '@clr/angular';
 
 
 @NgModule({
   declarations: [LoginPageComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ClarityModule,
     LoginRoutingModule
   ]
 })
